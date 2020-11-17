6ec9592c10c3b375b9b19959e6603c57ef8936b3
dmitry <ds1618033@gmail.com>
2020-11-17 13:22:25 -0500

    generate goal nodes

--- UCS.py
+++ UCS.py
@@ -1,4 +1,6 @@
-def uniform_cost_search(initial_node, goal_node):
-    children = initial_node.get_possible_children()
-    current_node = children.pop() # current_node = (1, node)
-    current_node.get_possible_children()
+    def uniform_cost_search(initial_node, goal_node):
+        children = initial_node.get_possible_children()
+        current_node = children.pop() # current_node = (1, node)
+        current_node.get_possible_children()
+
+    def is_goal_node
--- main.py
+++ main.py
@@ -2,26 +2,26 @@ import numpy as np
 from BoardNode import BoardNode
 from pretty_print import print_board_node
 
+height = 2
+width = 3
+
 def generate_sample(height, width):
     sample = np.arange(height*width)
     np.random.shuffle(sample)
     return sample.reshape((height, width))
 
 
+def generate_goal_nodes(height, width):
+    goal_1 = np.roll(np.arange(height*width), -1).reshape(height, width)
+    goal_2 = np.transpose(np.roll(np.arange(height*width), -1).reshape(width, height))
+    return goal_1, goal_2
 
-input_board = generate_sample(2, 4)
+
+input_board = generate_sample(height, width)
 starting_node = BoardNode(input_board)
 
-copied_array = np.copy(input_board)
+goal_1, goal_2 = generate_goal_nodes(height, width)
 
-print(BoardNode(input_board) == BoardNode(copied_array))
-
-
-#
-# print_board_node(starting_node)
-# children = starting_node.get_possible_children()
-# print()
-# while not children.empty():
-#     child = children.get()
-#     print_board_node(child.board)
-#     print()
+print_board_node(BoardNode(goal_1))
+print()
+print_board_node(BoardNode(goal_2))
